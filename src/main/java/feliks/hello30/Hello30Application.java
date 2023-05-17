322734b3df3537199dc7c5af11e042dbde53d4a5
Felix ToshibaSan <fiposa@inbox.ru>
2023-05-17 17:29:51 +0300

    www

---
+++ src/main/java/feliks/hello30/Hello30Application.java
@@ -0,0 +1,13 @@
+package feliks.hello30;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hello30Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hello30Application.class, args);
+    }
+
+}
---
+++ src/test/java/feliks/hello30/Hello30ApplicationTests.java
@@ -0,0 +1,13 @@
+package feliks.hello30;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hello30ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
